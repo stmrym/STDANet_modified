e68d991c7ff3337f6f2138f1520943914b453de5
stmrym <molly120420@gmail.com>
2024-02-14 18:59:42 +0900

    consider orthogonal edge

--- debug_2.py
+++ debug_2.py
@@ -4,6 +4,71 @@ import csv
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from typing import Optional, Union
+from tqdm import tqdm
+import math
+
+def radial_gradient(color,radii):
+    colors=[]
+    for r in radii:
+        colorr=r*color+(1-r)*np.array([1,1,1])
+        colors.append(colorr)
+    return colors
+
+
+def add_colors(colorwheel,cname,col,channel,key):
+    if key==0:
+        colorwheel[col:col+cname,channel]=255
+        colorwheel[col:col+cname,(channel+1)%3]=np.floor(255*np.arange(cname)/cname)
+    elif key==1:
+        colorwheel[col:col+cname,channel]=255-np.floor(255*np.arange(cname)/cname)
+        colorwheel[col:col+cname,(channel+1)%3]=255
+
+def gen_colorwheel():
+    RY,YG,GC,CB,BM,MR=15,6,4,11,13,6
+    ncols=RY+YG+GC+CB+BM+MR
+    colorwheel=np.zeros((ncols,3))
+    col=0
+
+    add_colors(colorwheel,RY,col,0,0)
+    col=col+RY
+    add_colors(colorwheel,YG,col,0,1)
+    col=col+YG
+    add_colors(colorwheel,GC,col,1,0)
+    col=col+GC
+    add_colors(colorwheel,CB,col,1,1)
+    col=col+CB
+    add_colors(colorwheel,BM,col,2,0)
+    col=col+BM
+    add_colors(colorwheel,MR,col,2,1)
+    return colorwheel/255.0
+
+def plot_colorwheel(colorwheel,steps=5, y_step=30):
+    fig = plt.figure(figsize=(3,3), dpi=300, tight_layout=True)
+    ax = fig.add_subplot(111, projection='polar')
+    ax.tick_params(bottom=False, left=False, right=False, top=False)
+    N=colorwheel.shape[0] # N = 55
+    x = np.linspace(0, 2*np.pi, N + 1)
+    y = np.linspace(0, 1, y_step)
+
+    for i in tqdm(range(N)):
+        
+        bins = np.linspace(x[i],x[i+1],steps)
+        colors = np.linspace(colorwheel[i],colorwheel[(i+1)%N],steps)
+
+        for j in range(steps):
+            color = colors[j]
+            line = radial_gradient(color, y)
+            
+            for k in range(len(y)):
+                ax.scatter(bins[j], y[k], c = [line[k]])
+    
+    ax.set_theta_direction(-1) 
+    ax.set_thetagrids([])
+    ax.set_rgrids([])
+    # ax.spines['polar'].set_visible(False)
+    # ax.axis("off")
+    plt.savefig('debug_results/colorwheel_2.png', transparent=True)
+
 
 def make_color_wheel(bins: Optional[Union[list, tuple]] = None) -> np.ndarray:
     """Build a color wheel.
@@ -84,7 +149,6 @@ def flow2rgb(flow: np.ndarray,
     rad = np.sqrt(dx**2 + dy**2)
     # [-1 ~ 1]の角度分布
     angle = np.arctan2(-dy, -dx) / np.pi
-
     # (h, w)
     bin_real = (angle + 1) / 2 * (num_bins - 1)
     bin_left = np.floor(bin_real).astype(int)
@@ -93,7 +157,7 @@ def flow2rgb(flow: np.ndarray,
     w = (bin_real - bin_left.astype(np.float32))[..., None]
     # 要素ごとの積
     flow_img = (1 - w) * color_wheel[bin_left, :] + w * color_wheel[bin_right, :]
-    
+
     # (h, w) True or False
     small_ind = rad <= 1
     
@@ -104,47 +168,151 @@ def flow2rgb(flow: np.ndarray,
     # (h,w,3) cは正規化したrgb値
     flow_map = np.uint8(flow_img * 255.)
     flow_map = cv2.cvtColor(flow_map, cv2.COLOR_RGB2BGR)
-    cv2.imwrite('debug_results/flow_4.png', flow_map)
+    cv2.imwrite('debug_results/twmp.png', flow_map)
 
     return flow_img
     
 
 
+def flow2direction(flow: np.ndarray,
+            color_wheel: Optional[np.ndarray] = None,
+            unknown_thr: float = 1e6) -> np.ndarray:
+    """Convert flow map to RGB image.
 
-path = 'exp_log/train/2024-01-29T051821_STDAN_Stack_BSD_3ms24ms_GOPRO/visualization/epoch-0350/out_flow_npy/027/00000024.npy'
+    Args:
+        flow (ndarray): Array of optical flow.
+        color_wheel (ndarray or None): Color wheel used to map flow field to
+            RGB colorspace. Default color wheel will be used if not specified.
+        unknown_thr (float): Values above this threshold will be marked as
+            unknown and thus ignored.
+
+    Returns:
+        ndarray: RGB image that can be visualized.
+    """
+    assert flow.ndim == 3 and flow.shape[-1] == 2
+    if color_wheel is None:
+        color_wheel = make_color_wheel()
+    assert color_wheel.ndim == 2 and color_wheel.shape[1] == 3
+    num_bins = color_wheel.shape[0]
+
+    dx = flow[:, :, 0].copy()
+    dy = flow[:, :, 1].copy()
+
+    ignore_inds = (
+        np.isnan(dx) | np.isnan(dy) | (np.abs(dx) > unknown_thr) |
+        (np.abs(dy) > unknown_thr))
+    dx[ignore_inds] = 0
+    dy[ignore_inds] = 0
+
+    rad = np.sqrt(dx**2 + dy**2)
+    if np.any(rad > np.finfo(float).eps):
+        max_rad = np.max(rad)
+        dx /= max_rad
+        dy /= max_rad
+
+    # [0, 1]に正規化した強度分布 (h,w)
+    rad = np.sqrt(dx**2 + dy**2)
+    # [-1 ~ 1]の角度分布
+    angle = np.arctan(dy/(dx + 1e-8)) * 2 / np.pi
+        # (h, w)
+    bin_real = (angle + 1) / 2 * (num_bins - 1)
+    bin_left = np.floor(bin_real).astype(int)
+    bin_right = (bin_left + 1) % num_bins
+    # (h, w, 1)
+    w = (bin_real - bin_left.astype(np.float32))[..., None]
+    # 要素ごとの積
+    flow_img = (1 - w) * color_wheel[bin_left, :] + w * color_wheel[bin_right, :]
+
+    # (h, w) True or False
+    small_ind = rad <= 1
+    
+    flow_img[small_ind] = 1 - rad[small_ind, None] * (1 - flow_img[small_ind])
+    flow_img[np.logical_not(small_ind)] *= 0.75
+
+    flow_img[ignore_inds, :] = 0
+    # (h,w,3) cは正規化したrgb値
+    flow_map = np.uint8(flow_img * 255.)
+    flow_map = cv2.cvtColor(flow_map, cv2.COLOR_RGB2BGR)
+    cv2.imwrite('debug_results/flow_half.png', flow_map)
+
+    return flow_img
+
+
+
+
+
+path = './debug_results/000198.npy'
 flow = np.load(path)
 
-# flow2rgb(flow)
 
-img = cv2.imread('../dataset/GOPRO_Large/test/GOPR0854_11_00/sharp/000008.png', cv2.IMREAD_GRAYSCALE)
-print(type(img[0,0]))
+
+
+# path = '../dataset/GOPRO_Large/test/GOPR0410_11_00/sharp/000198.png'
+path = './exp_log/train/WO_Motion_2024-01-16T103421_STDAN_Stack_BSD_3ms24ms_GOPRO/visualization/epoch-0350/output/GOPR0410_11_00/000198.png'
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
 
 # img = np.full((512, 512), 0, dtype=np.uint8)
 # cv2.circle(img, (256,256), 150, (255,255), thickness=-1)
 
 
+cv2.imwrite('./debug_results/output_gray.png', img)
+# exit()
+
 sobel_x_kernel = np.array(
-    [[1, 0, -1],
-     [2, 0, -2],
-     [1, 0, -1]]
+    [[-1, 0, 1],
+     [-2, 0, 2],
+     [-1, 0, 1]]
     )
 
 sobel_y_kernel = np.array(
-    [[1, 2, 1],
+    [[-1, -2, -1],
      [0, 0, 0],
-     [-1, -2, -1]
+     [1, 2, 1]
     ]
     )
 
 
+
+
+
+
 sobel_x = cv2.filter2D(img, cv2.CV_64F, sobel_x_kernel)
 sobel_y= cv2.filter2D(img, cv2.CV_64F, sobel_y_kernel)
+
 amp = np.sqrt(sobel_x**2 + sobel_y**2)
+# [0, 1] normalized
+amp /= np.max(amp)
 
 
-edge_flow = np.stack([sobel_x, sobel_y], axis=-1)
+edge_direction = np.stack([sobel_x, sobel_y], axis=-1)
 
-angle = flow2rgb(edge_flow)
+if flow.shape != edge_direction.shape:
+    flow = cv2.resize(flow, edge_direction.shape[1::-1])
+
+
+prod = np.multiply(flow[:,:,0], edge_direction[:,:,0]) + np.multiply(flow[:,:,1], edge_direction[:,:,1])
+abs_prod = np.abs(prod)
+# [0, 1] normalized
+abs_prod /= np.max(abs_prod)
+
+orthogonal_edge = np.multiply(abs_prod, amp)
+
+
+amp = np.uint8(amp/np.max(amp) * 255.)
+abs_prod = np.uint8(abs_prod/np.max(abs_prod) * 255.)
+orthogonal_edge = np.uint8(orthogonal_edge/np.max(orthogonal_edge) * 255.)
+
+cv2.imwrite('./debug_results/sobel_amp.png', amp)
+cv2.imwrite('./debug_results/sobel_amp_map.png', abs_prod)
+cv2.imwrite('./debug_results/sobel_orth_edge.png', orthogonal_edge)
+
+
+exit()
+
+
+# angle = flow2rgb(edge_flow)
+edge_half_angle = flow2direction(edge_angle)
+
 
 
 # angle = np.arctan2(-sobel_y, -sobel_x)/ np.pi
@@ -152,19 +320,19 @@ angle = flow2rgb(edge_flow)
 # print(amp)
 
 
-fig, ax = plt.subplots(tight_layout=True, dpi=200)
-im = ax.imshow(angle, vmin=-180, vmax=180, cmap='hsv', aspect='equal')
+# fig, ax = plt.subplots(tight_layout=True, dpi=200)
+# im = ax.imshow(angle, vmin=0, vmax=360, cmap='hsv', aspect='equal')
 
-divider = make_axes_locatable(ax)
-cax = divider.append_axes("top", size="5%", pad=0.3)
+# divider = make_axes_locatable(ax)
+# cax = divider.append_axes("top", size="5%", pad=0.3)
 
-fig.colorbar(im, cax=cax, orientation='horizontal')
-plt.savefig('./debug_results/angle_2.png')
+# fig.colorbar(im, cax=cax, orientation='horizontal')
+# plt.savefig('./debug_results/angle_2.png')
 
 # with open('./debug_results/angle.csv', 'w', newline='') as file:
     # writer = csv.writer(file)
     # writer.writerows(angle)
 
-# cv2.imwrite('./debug_results/output_gray.png', img)
+cv2.imwrite('./debug_results/output_gray.png', img)
 # cv2.imwrite('./debug_results/output_x.png', sobel_x)
 # cv2.imwrite('./debug_results/output_y.png', sobel_y)
