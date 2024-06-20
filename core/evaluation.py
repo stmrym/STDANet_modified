14cd745d613b2d58df372b817fa75aa5ca221d0b
stmrym <molly120420@gmail.com>
2024-06-20 18:11:01 +0900

    delete saving mid results

--- core/evaluation.py
+++ core/evaluation.py
@@ -16,8 +16,8 @@ from utils.util import ssim_calculate
 import pandas as pd
 from tqdm import tqdm
 
-def make_eval_df(cfg, epoch_average_list: list, save_name: str) -> pd.DataFrame:
-    col_name = ['seq', 'mid_SSIM', 'out_SSIM', 'mid_SSIM_sd', 'out_SSIM_sd'] if cfg.NETWORK.USE_STACK else ['seq', 'out_SSIM', 'out_SSIM_sd']
+def make_eval_df(epoch_average_list: list, save_name: str) -> pd.DataFrame:
+    col_name = ['seq', 'out_SSIM', 'out_SSIM_sd']
     eval_df = pd.DataFrame(epoch_average_list, columns=col_name)
     for col in col_name[1:]:
         eval_df.at['Avg.', col] = eval_df[col].mean()
@@ -29,20 +29,17 @@ def make_eval_df(cfg, epoch_average_list: list, save_name: str) -> pd.DataFrame:
     
     return eval_df
 
-def add_epoch_average_list(cfg, epoch_average_list: list, seq_df: pd.DataFrame) -> list:
-    if cfg.NETWORK.USE_STACK:
-        epoch_average_list.append([seq_df['seq'][0], seq_df['mid_SSIM'].mean(), seq_df['out_SSIM'].mean(), np.sqrt(seq_df['mid_SSIM'].var()), np.sqrt(seq_df['out_SSIM'].var())])
-    else:
-        epoch_average_list.append([seq_df['seq'][0], seq_df['out_SSIM'].mean(), np.sqrt(seq_df['out_SSIM'].var())])
+def add_epoch_average_list(epoch_average_list: list, seq_df: pd.DataFrame) -> list:
+    epoch_average_list.append([seq_df['seq'][0], seq_df['out_SSIM'].mean(), np.sqrt(seq_df['out_SSIM'].var())])
     return epoch_average_list
 
-def make_seq_df(cfg, seq_frame_value_list: list, epoch_average_list: list, save_dir: str, save_name: str) -> list:
-    col_name = ['seq', 'frame', 'mid_SSIM', 'out_SSIM'] if cfg.NETWORK.USE_STACK else ['seq', 'frame', 'out_SSIM']
+def make_seq_df(seq_frame_value_list: list, epoch_average_list: list, save_dir: str, save_name: str) -> list:
+    col_name = ['seq', 'frame', 'out_SSIM']
     seq_df = pd.DataFrame(seq_frame_value_list, columns=col_name)
     if not os.path.isdir(os.path.join(save_dir)):
         os.makedirs(save_dir, exist_ok=True)
     seq_df.to_csv(os.path.join(save_dir, save_name), index=False)
-    epoch_average_list = add_epoch_average_list(cfg, epoch_average_list, seq_df)
+    epoch_average_list = add_epoch_average_list(epoch_average_list, seq_df)
     return epoch_average_list
 
 def save_feat_grid(feat: torch.Tensor, save_name: str, nrow: int = 1) -> None:
@@ -82,14 +79,10 @@ def evaluation(cfg,
 
     inference_time = utils.network_utils.AverageMeter()
     process_time   = utils.network_utils.AverageMeter()
-    if cfg.EVAL.CALC_METRICS:
-        img_PSNRs_out = utils.network_utils.AverageMeter()
-        img_LPIPSs_out = utils.network_utils.AverageMeter()
-        loss_fn_alex = lpips.LPIPS(net='alex').cuda()
+    img_PSNRs_out = utils.network_utils.AverageMeter()
+    img_LPIPSs_out = utils.network_utils.AverageMeter()
+    loss_fn_alex = lpips.LPIPS(net='alex').cuda()
 
-    if cfg.NETWORK.USE_STACK:    
-        img_PSNRs_mid = utils.network_utils.AverageMeter()
-        img_LPIPSs_mid = utils.network_utils.AverageMeter()
 
     losses_dict_list = []
     for loss_config_dict in cfg.LOSS_DICT_LIST:
@@ -155,12 +148,6 @@ def evaluation(cfg,
 
             output_ndarrays = output_dict['out']['final'].detach().cpu().permute(0,2,3,1).numpy()*255
             gt_ndarrays = gt_tensor.detach().cpu().permute(0,2,3,1).numpy()*255        
-            
-            if cfg.NETWORK.USE_STACK:
-                if cfg.EVAL.CALC_METRICS: 
-                    img_PSNRs_mid.update(util.calc_psnr(output_dict['out']['recons_2'].detach(),gt_tensor.detach()), cfg.CONST.EVAL_BATCH_SIZE)
-                    img_LPIPSs_mid.update(loss_fn_alex(output_dict['out']['recons_2'], gt_tensor).mean().detach().cpu(), cfg.CONST.EVAL_BATCH_SIZE)
-                mid_ndarrays = output_dict['out']['recons_2'].detach().cpu().permute(0,2,3,1).numpy()*255
 
             for batch in range(0, output_ndarrays.shape[0]):
                 
@@ -171,7 +158,7 @@ def evaluation(cfg,
                     elif seq != name[batch].split('.')[0]:
                             # End of sequence, and make dataFrame
                             csv_savedir = save_dir +  '_csv'
-                            epoch_average_list = make_seq_df(cfg, seq_frame_value_list, epoch_average_list, csv_savedir, str(epoch_idx) + '_' + seq + '.csv')
+                            epoch_average_list = make_seq_df(seq_frame_value_list, epoch_average_list, csv_savedir, str(epoch_idx) + '_' + seq + '.csv')
                             # Initialize for next sequence
                             seq_frame_value_list = []
                 seq, img_name = name[batch].split('.')  # name = ['000.00000002']
@@ -181,18 +168,9 @@ def evaluation(cfg,
                 output_image_bgr = cv2.cvtColor(np.clip(output_ndarr, 0, 255).astype(np.uint8), cv2.COLOR_RGB2BGR)
                 gt_image_bgr = cv2.cvtColor(np.clip(gt_ndarr, 0, 255).astype(np.uint8), cv2.COLOR_RGB2BGR)
                 
-                if cfg.NETWORK.USE_STACK:
-                    mid_ndarr = mid_ndarrays[batch,:,:,:]
-                    mid_image_bgr = cv2.cvtColor(np.clip(mid_ndarr, 0, 255).astype(np.uint8), cv2.COLOR_RGB2BGR)
-                
                 if cfg.EVAL.CALC_METRICS:
                     img_ssim_out = ssim_calculate(output_image_bgr, gt_image_bgr)
-
-                    if cfg.NETWORK.USE_STACK:
-                        img_ssim_mid = ssim_calculate(mid_image_bgr, gt_image_bgr)
-                        seq_frame_value_list.append([seq, int(img_name), img_ssim_mid, img_ssim_out])
-                    else:
-                        seq_frame_value_list.append([seq, int(img_name), img_ssim_out])
+                    seq_frame_value_list.append([seq, int(img_name), img_ssim_out])
 
                 if cfg.NETWORK.PHASE == 'test':
                     cfg.EVAL.VISUALIZE_FREQ = 1
@@ -204,12 +182,6 @@ def evaluation(cfg,
                         os.makedirs(os.path.join(save_dir + '_output', seq), exist_ok=True)
                     
                     cv2.imwrite(os.path.join(save_dir + '_output', seq, img_name + '.png'), output_image_bgr)
-
-                    if cfg.NETWORK.USE_STACK:
-                        if not os.path.isdir(os.path.join(save_dir + '_mid', seq)):
-                            os.makedirs(os.path.join(save_dir + '_mid', seq), exist_ok=True)               
-                        cv2.imwrite(os.path.join(save_dir + '_mid', seq, img_name + '.png'), mid_image_bgr)
-
 
                     # save_feat_grid((output_dict['first_scale_inblock']['final'])[batch,1], save_dir + f'{seq}_{img_name}_0_in_feat', nrow=4)
                     # save_feat_grid((output_dict['first_scale_encoder_first']['final'])[batch,1], save_dir + f'{seq}_{img_name}_1_en_feat', nrow=8)
@@ -246,22 +218,16 @@ def evaluation(cfg,
             torch.cuda.synchronize()
             process_time.update((time() - process_start_time))
             if cfg.EVAL.CALC_METRICS:
-                if cfg.NETWORK.USE_STACK:
-                    tqdm_eval.set_postfix_str(f'Inference Time {inference_time} Process Time {process_time} PSNR_mid {img_PSNRs_mid} PSNR_out {img_PSNRs_out}')
-                else:
-                    tqdm_eval.set_postfix_str(f'Inference Time {inference_time} Process Time {process_time} PSNR_out {img_PSNRs_out}')
+                tqdm_eval.set_postfix_str(f'Inference Time {inference_time} Process Time {process_time} PSNR_out {img_PSNRs_out}')
             else:
-                if cfg.NETWORK.USE_STACK:
-                    tqdm_eval.set_postfix_str(f'Inference Time {inference_time} Process Time {process_time}')
-                else:
-                    tqdm_eval.set_postfix_str(f'Inference Time {inference_time} Process Time {process_time}')
+                tqdm_eval.set_postfix_str(f'Inference Time {inference_time} Process Time {process_time}')
     
     if cfg.EVAL.CALC_METRICS:
         # Make dataFrame of last sequence    
         csv_savedir = save_dir +  '_csv'            
-        epoch_average_list = make_seq_df(cfg, seq_frame_value_list, epoch_average_list, csv_savedir, str(epoch_idx) + '_' + seq + '.csv')
+        epoch_average_list = make_seq_df(seq_frame_value_list, epoch_average_list, csv_savedir, str(epoch_idx) + '_' + seq + '.csv')
         # Make average dataFrame at the epoch
-        eval_df = make_eval_df(cfg, epoch_average_list, save_dir + '_average.csv')
+        eval_df = make_eval_df(epoch_average_list, save_dir + '_average.csv')
     
     # Add testing results to TensorBoard
     if cfg.NETWORK.PHASE in ['train', 'resume']:
@@ -285,18 +251,11 @@ def evaluation(cfg,
             tb_writer.add_scalar(f'LPIPS/VALID_{eval_dataset_name}', img_LPIPSs_out.avg, epoch_idx)
 
     if cfg.EVAL.CALC_METRICS:
-        if cfg.NETWORK.USE_STACK:
-            log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] PSNR(mid:{img_PSNRs_mid.avg}, out:{img_PSNRs_out.avg}), PSNR_best:{Best_Img_PSNR} at epoch {Best_Epoch}')
-            log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] SSIM(mid:{eval_df.at["Avg.","mid_SSIM"]}, out:{eval_df.at["Avg.","out_SSIM"]}), Infer. time:{inference_time}, Process time:{process_time}')
-        else:
-            log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] PSNR(out:{img_PSNRs_out.avg}), PSNR_best:{Best_Img_PSNR} at epoch {Best_Epoch}')
-            log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] SSIM(out:{eval_df.at["Avg.","out_SSIM"]}), Infer. time:{inference_time}, Process time:{process_time}')
+        log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] PSNR(out:{img_PSNRs_out.avg}), PSNR_best:{Best_Img_PSNR} at epoch {Best_Epoch}')
+        log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] SSIM(out:{eval_df.at["Avg.","out_SSIM"]}), Infer. time:{inference_time}, Process time:{process_time}')
 
     else:
-        if cfg.NETWORK.USE_STACK:
-            log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] Infer. time:{inference_time}, Process time:{process_time}')
-        else:
-            log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] Infer. time:{inference_time}, Process time:{process_time}')       
+        log.info(f'[EVAL][Epoch {epoch_idx}/{cfg.TRAIN.NUM_EPOCHES}][{eval_dataset_name}] Infer. time:{inference_time}, Process time:{process_time}')       
 
 
     return Best_Img_PSNR, Best_Epoch
